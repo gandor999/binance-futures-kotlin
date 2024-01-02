8c677d8f059f5719a8b428cb4135673c24b3c5d7
Geodor Ruales <gandor999@gmail.com>
2024-01-03 00:51:15 +0800

    feat: add utility functions

---
+++ src/main/kotlin/util.kt
@@ -0,0 +1,32 @@
+import com.binance.connector.futures.client.impl.UMFuturesClientImpl
+import data.dto.SetProspectsArgs
+import data.models.BinanceException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+
+fun handleError(runCatchObject: Result<String>) {
+    runCatchObject.onFailure { error ->
+        val deserializedError = Json.decodeFromString<BinanceException>(error.message.toString())
+
+        when (deserializedError.code) {
+            -4046 -> println(deserializedError.msg)
+
+            else -> throw Exception(error.message)
+        }
+    }
+}
+
+fun setProspects(client: UMFuturesClientImpl, setProspectsArgs: SetProspectsArgs) = runBlocking {
+    launch {
+        runCatching {
+            client.account().changeMarginType(setProspectsArgs.marginParam)
+        }.also { handleError(it) }
+    }
+
+    launch {
+        runCatching {
+            client.account().changeInitialLeverage(setProspectsArgs.leverageParam)
+        }.also { handleError(it) }
+    }
+}
