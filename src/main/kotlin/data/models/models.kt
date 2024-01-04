8b5da1e712fe3a651c29b677b4a1035cd2216024
Geodor Ruales <gandor999@gmail.com>
2024-01-04 23:44:12 +0800

    feat: add Ticker24H

--- src/main/kotlin/data/models/models.kt
+++ src/main/kotlin/data/models/models.kt
@@ -27,3 +27,23 @@ data class TickerSymbol(
     val price: String,
     val time: Long,
 )
+
+@Serializable
+data class Ticker24H(
+    val symbol: String,
+    val priceChange: String,
+    val priceChangePercent: String,
+    val weightedAvgPrice: String,
+    val lastPrice: String,
+    val lastQty: String,
+    val openPrice: String,
+    val highPrice: String,
+    val lowPrice: String,
+    val volume: String,
+    val quoteVolume: String,
+    val openTime: Long,
+    val closeTime: Long,
+    val firstId: Long,
+    val lastId: Long,
+    val count: Int
+)
