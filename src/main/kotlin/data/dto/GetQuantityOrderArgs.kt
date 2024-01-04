7ce3b250c939a645769b77cb0a61cc2b4de9074f
Geodor Ruales <gandor999@gmail.com>
2024-01-04 23:43:22 +0800

    chore: delete or rename these

--- src/main/kotlin/data/dto/GetQuantityOrderArgs.kt
+++
@@ -1,7 +0,0 @@
-package data.dto
-
-data class GetQuantityOrderArgs(
-    val futuresAccountBalanceParam: LinkedHashMap<String, Any>,
-    val leverageParam: LinkedHashMap<String, Any>,
-    val regularFee: Double
-)
--- src/main/kotlin/data/singletons/UMFuturesClient.kt
+++
@@ -1,8 +0,0 @@
-package data.singletons
-
-import com.binance.connector.futures.client.impl.UMFuturesClientImpl
-import data.secrets.BinanceSecrets
-
-object UMFutures {
-    val client = UMFuturesClientImpl(BinanceSecrets.getAPIKey(), BinanceSecrets.getAPISecret())
-}
